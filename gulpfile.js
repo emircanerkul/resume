3ae2a5b2bcf78d40eedb3288f710e061669ad56c
emircanerkul <develop@emircanerkul.com>
2021-06-20 09:45:13 +0000

    sw-precache to workbox-build for generating pwa

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,37 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const workboxBuild = require('workbox-build');
 
-gulp.task('generate-service-worker', function(callback) {
-  var path = require('path');
-  var swPrecache = require('sw-precache');
+gulp.task('generate-service-worker', () => {
   var rootDir = 'themes/hugo-devresume-theme/static';
+  var swDest = rootDir + '/sw.js';
+  
+  return workboxBuild.generateSW({
+    globDirectory: 'docs',
+    globPatterns: [
+      '**/*.{js,html,css,png,jpg,gif,eot,svg,ttf,woff,woff2,otf}',
+    ],
+    swDest: swDest,
 
-  swPrecache.write(path.join(rootDir, 'sw.js'), {
-    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,eot,svg,ttf,woff,woff2,otf}'],
-    stripPrefix: rootDir
-  }, callback);
+    runtimeCaching: [{
+      urlPattern: /\.(?:html)$/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "markup",
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 7,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      handler: 'CacheFirst',
+
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 10,
+        },
+      },
+    }],
+  });
 });
