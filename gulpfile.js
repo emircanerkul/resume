198dd4cfed0aae46fd549e1d8d354ed19f443931
emircanerkul <develop@emircanerkul.com>
2021-06-20 07:58:15 +0000

    pwa with sw-precache, npm init, gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+  var rootDir = 'themes/hugo-devresume-theme/static';
+
+  swPrecache.write(path.join(rootDir, 'sw.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,eot,svg,ttf,woff,woff2,otf}'],
+    stripPrefix: rootDir
+  }, callback);
+});
